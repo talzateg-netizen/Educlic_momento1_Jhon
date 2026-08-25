d11cdd28459f158b881fdd7e51d35e1b31ebd589
talzateg-netizen <talzateg@gmail.com>
2026-08-24 20:30:12 -0500

    creación proyecto

---
+++ src/test/java/com/cesde/educlic/EduclicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesde.educlic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduclicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
